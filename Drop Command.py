b08d1c60a2e5e2e2b3e7b99b4383b12679489a35
shflshty <78846663+ShefaliShettyy@users.noreply.github.com>
2021-02-10 20:04:39 +0530

    Update Drop Command.py

--- Drop Command.py
+++ Drop Command.py
@@ -1,75 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[41]:
-
-
 import pandas as pd
 import numpy as np
-
-
-# In[42]:
-
-
 df =pd.DataFrame(np.arange(12).reshape(3, 4), columns=['P', 'Q', 'R', 'S'])
-
-
-# In[43]:
-
-
 df
-
-
-# In[44]:
-
-
-df.drop(['Q','R'],axis=1)
-
-
-# In[45]:
-
-
+df.drop(['Q','R'],axis=1)  #columns
 df.drop(columns=['P', 'S'])
-
-
-# In[46]:
-
-
-df.drop([0, 1])
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+df.drop([0, 1])  #row
